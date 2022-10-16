beb6a029d5743ee83a6883cf113b7dafade8c01a
Onojo Ameh <amehonoj@gmail.com>
2022-10-16 11:48:53 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+ #include <stdio.h>
 
 /**
  * main - Prints all possible combinations of single-digit numbers.
@@ -14,6 +14,8 @@ int main(void)
 		putchar((num % 10) + '0');
 		if (num == 9)
 			continue;
+		putchar(',');
+		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
