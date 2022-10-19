649a36cca454012df682a1226e9d15ee24df0f7a
Onojo Ameh <amehonoj@gmail.com>
2022-10-18 21:58:47 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,21 @@
 #include "main.h"
 
 /**
- * main - Print _putchar, followed by a new line
+ * main - Print _putchar
  *
- * Return: 0 (success)
+ * Return: Always 0
  */
+
 int main(void)
 {
-	char *sh = "main.h"
-
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
